d0e36adc926a08128ef155bd4c26cf929eec76a9
Guilherme Lopes <gui835486@gmail.com>
2025-09-01 17:48:05 -0300

    Fase final do jogo onde você conseue escolher os tributos a ser comparados

--- super_trunfo.c
+++ super_trunfo.c
@@ -115,7 +115,7 @@ int main() {
 
     printf("\nComparação de Cartas:\n");
     printf("População: ");
-    if (populacao > pontoturistico2){
+    if (populacao > populacao2){
         printf("Carta 1 venceu!\n");
     }else{
         printf("Carta 2 venceu\n");
@@ -207,7 +207,7 @@ int main() {
             printf("%s venceu \n", nomecidade2);
         }else{
             printf("Empate!!");
-            break;
+        break;
         }
 
     case 5:
@@ -223,7 +223,7 @@ int main() {
         printf("\n");
 
         printf("==== Escolha o segundo atributo de comparação ==== \n");
-        for(int i = 1; i <= 6; i++){
+        for(int i = 1; i <= 5; i++){
             if (i != atributo1){
                 switch (i){
                 case 1: printf("1 - Popoulação\n ", i);break;
@@ -243,6 +243,77 @@ int main() {
         }
         printf("Escolha\n");
         scanf("%d", &atributo2);
+
+        float valor1_carta1, valor2_carta1;
+        switch (atributo1) {
+            case 1: valor1_carta1 = populacao; break;
+            case 2: valor1_carta1 = pib; break;
+            case 3: valor1_carta1 = pontoturistico; break;
+            case 4: valor1_carta1 = densidadepopulacional; break;
+            case 5: valor1_carta1 = pibpercapita; break;
+        }
+        switch (atributo2) {
+            case 1: valor2_carta1 = populacao; break;
+            case 2: valor2_carta1 = pib; break;
+            case 3: valor2_carta1 = pontoturistico; break;
+            case 4: valor2_carta1 = densidadepopulacional; break;
+            case 5: valor2_carta1 = pibpercapita; break;
+        }
+
+        // Pegando valores dos atributos para carta 2
+        float valor1_carta2, valor2_carta2;
+        switch (atributo1) {
+            case 1: valor1_carta2 = populacao2; break;
+            case 2: valor1_carta2 = pib2; break;
+            case 3: valor1_carta2 = pontoturistico2; break;
+            case 4: valor1_carta2 = densidadepopulacional2; break;
+            case 5: valor1_carta2 = pibpercapita2; break;
+        }
+        switch (atributo2) {
+            case 1: valor2_carta2 = populacao2; break;
+            case 2: valor2_carta2 = pib2; break;
+            case 3: valor2_carta2 = pontoturistico2; break;
+            case 4: valor2_carta2 = densidadepopulacional2; break;
+            case 5: valor2_carta2 = pibpercapita2; break;
+        }
+
+        // Mostrar os valores
+        printf("\n==== VALORES COMPARADOS ====\n");
+        printf("%s: %.2f e %.2f\n", nomecidade, valor1_carta1, valor2_carta1);
+        printf("%s: %.2f e %.2f\n", nomecidade2, valor1_carta2, valor2_carta2);
+
+        // Regra especial para densidade populacional (menor vence)
+        int carta1_vitorias = 0;
+        int carta2_vitorias = 0;
+
+        if (atributo1 == 4) {
+            (valor1_carta1 < valor1_carta2) ? carta1_vitorias++ : carta2_vitorias++;
+        } else {
+            (valor1_carta1 > valor1_carta2) ? carta1_vitorias++ : carta2_vitorias++;
+        }
+
+        if (atributo2 == 4) {
+            (valor2_carta1 < valor2_carta2) ? carta1_vitorias++ : carta2_vitorias++;
+        } else {
+            (valor2_carta1 > valor2_carta2) ? carta1_vitorias++ : carta2_vitorias++;
+        }
+
+        // Soma total dos dois atributos para cada carta
+        float soma1 = valor1_carta1 + valor2_carta1;
+        float soma2 = valor1_carta2 + valor2_carta2;
+
+        printf("\n==== RESULTADO ====\n");
+        printf("Soma dos atributos:\n");
+        printf("%s: %.2f\n", nomecidade, soma1);
+        printf("%s: %.2f\n", nomecidade2, soma2);
+
+        if (soma1 > soma2) {
+            printf(">> %s venceu com maior soma de atributos!\n", nomecidade);
+        } else if (soma2 > soma1) {
+            printf(">> %s venceu com maior soma de atributos!\n", nomecidade2);
+        } else {
+            printf(">> Empate!\n");
+        }
 
         break;
 
